c69358b7c7db3d40fc05245263d37f50077a39ef
Igor Shtelmakh <igor.shtelmakh@gmail.com>
2018-06-29 00:19:51 +0300

    auth

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalog.apps.CatalogConfig',     
+    'catalog.apps.CatalogConfig',        
 ]
 
 MIDDLEWARE = [
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
